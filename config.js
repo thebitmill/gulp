907df1b2dda3d02eea36adfd8585608d727bf310
Linus Miller <lohfu@lohfu.io>
2016-09-15 11:26:43 +0200

    Merge local config with default config

--- config.js
+++ config.js
@@ -1,15 +1,22 @@
 'use strict'
 
+// modules > native
 const p = require('path')
 
+//modules > 3rd party
 const chalk = require('chalk')
+const _ = require('lodash')
 
+// modules > gulp
 const gutil = require('gulp-util')
 
-const localConfig = p.join(process.cwd(), 'gulpconfig.js')
+const localConfigPath = p.join(process.cwd(), 'gulpconfig.js')
 
 try {
-  module.exports = require(localConfig)
+  const localConfig = require(localConfigPath)
+
+  module.exports = _.mergeWith(require('./default-config'), localConfig, (a, b) => Array.isArray(a) ? b : undefined)
+
   gutil.log('Local ' + chalk.magenta('gulpconfig.js') + ' found and loaded')
 } catch (e) {
   if (e.message && !e.message.startsWith('Cannot find module'))
